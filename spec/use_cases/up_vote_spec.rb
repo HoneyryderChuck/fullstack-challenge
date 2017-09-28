ff0c13d79a45e6a21ba8f3b0015461b9d365c959
Tiago Cardoso <tiago@restorm.com>
2017-09-28 11:30:20 +0100

    added a spec to show the bug (that, when the upvote doesn't exist, it should create it)

--- spec/use_cases/up_vote_spec.rb
+++ spec/use_cases/up_vote_spec.rb
@@ -3,16 +3,25 @@ require 'rails_helper'
 RSpec.describe FetchComics do
 
   context 'when a vote is given' do
-    before do
-      ComicVote.create(comic_id: 123)
-    end
 
-    it 'increases votes amount if ComicVote exists for the comic_id' do
-      UpVote.perform(123)
+		it 'creates the comic vote on the fly' do
+			expect {
+				UpVote.perform(123)
+			}.to change(ComicVote, :count).by(1)
+		end
 
-      comic_votes = ComicVote.find_by(comic_id: 123)
+		context 'and comic vote already exists' do
+    	before do
+    	  ComicVote.create(comic_id: 123)
+    	end
 
-      expect(comic_votes.votes).not_to be_nil
-    end
+    	it 'increases votes amount if ComicVote exists for the comic_id' do
+    	  UpVote.perform(123)
+
+    	  comic_votes = ComicVote.find_by(comic_id: 123)
+
+    	  expect(comic_votes.votes).not_to be_nil
+    	end
+		end
   end
 end
