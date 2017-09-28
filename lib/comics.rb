0d03904baf73ac7337e974f6dda35006c95ebc9f
Tiago Cardoso <tiago@restorm.com>
2017-09-28 19:08:22 +0100

    added fetch method to fetch a single comic

--- lib/comics.rb
+++ lib/comics.rb
@@ -4,9 +4,17 @@ class Comics
     new.search(search_params)
   end
 
+  def self.fetch(*args)
+		new.fetch(*args)
+  end
+
   def initialize(api_client = Marvel::Client.new)
     @api_client = api_client
   end
+
+	def fetch(id, search_params)
+		@api_client.comic(id, search_params)
+	end
 
   def search(search_params)
     search_params[:offset] = page_size * search_params.delete(:page).to_i
